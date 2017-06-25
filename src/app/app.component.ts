1c60974a0fbe47ac0d7336db16694bef0eca7ce3
Julian Silva <julian.silva@correounivalle.edu.co>
2017-06-25 01:16:04 +0100

    adding navbar and components material design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+import {MdMenuTrigger} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  routes: Object[] = [
+{
+title: 'Top Movies',
+route: '/',
+icon: 'local_movies',
+}, {
+title: 'Genres',
+route: '/',
+icon: 'view_carousel',
+}, {
+title: 'People',
+route: '/',
+icon: 'person',
+}, {
+title: 'Tv - Series',
+route: '/',
+icon: 'tv',
+}, {
+title: 'A - Z List',
+route: '/',
+icon: 'sort_by_alpha',
+},
+];
+
+  @ViewChild(MdMenuTrigger) trigger: MdMenuTrigger;
+
+  someMethod() {
+    this.trigger.openMenu();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { AlertModule } from 'ngx-bootstrap';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 import {MdMenuModule} from '@angular/material';
+import{MdIconModule} from '@angular/material';
+import {MdSidenavModule} from '@angular/material';
+import {MdToolbarModule} from '@angular/material';
+import {MdTabsModule} from '@angular/material';
+import {MdButtonToggleModule} from '@angular/material';
+import {MdListModule} from '@angular/material';
+
+
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -16,10 +26,16 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     MdButtonModule, 
     MdCheckboxModule,
+    MdIconModule,
+    MdMenuModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdTabsModule,
+    MdButtonToggleModule,
+    MdListModule,
+    
       ],
-  providers: [
-  
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 
 })
